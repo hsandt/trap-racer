7894c353bb1a4aa002f8a91b3402417db230826b
Yuiyui-lightning <midou.yui@gmail.com>
2020-10-18 01:24:13 +0900

    [Stage]Background_Update

---
+++ Assets/Scenes/background/laser/anime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser: MonoBehaviour
+{
+
+    public float interval = 0.1f;
+
+    void Start()
+    {
+        StartCoroutine("Blink");
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            var renderComponent = GetComponent<Renderer>();
+            renderComponent.enabled = !renderComponent.enabled;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+}
