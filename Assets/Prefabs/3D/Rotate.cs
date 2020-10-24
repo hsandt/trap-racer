5feecd722d117f3570d24de15add70763864c5e9
Yuiyui-lightning <midou.yui@gmail.com>
2020-10-25 03:41:02 +0900

    [Design]StageData,CharacterData

---
+++ Assets/Prefabs/3D/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = new Vector3(0, 2f, 0);
+	}
+
+}
