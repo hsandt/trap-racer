a675ae0e4f4d983f3634030406ba8c25a40c39be
komehara <komehara@gamedesignshort.com>
2020-10-25 17:05:10 +0100

    [TRAMPOLINE] Added Trampoline behavior (trigger jump at given speed Y)

---
+++ Assets/Scripts/Stage/Trampoline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CommonsHelper;
+
+public class Trampoline : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var characterRun = other.GetComponentOrFail<CharacterRun>();
+        characterRun.JumpWithTrampoline();
+    }
+
+    private void StartAnimation()
+    {
+        // TODO
+    }
+}
