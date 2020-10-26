7926d2baf52e7c442125e35a2708f153ff7c9d29
huulong <n.huu.long@gmail.com>
2020-10-26 20:54:57 +0100

    [CONVEYOR BELT] Added Conveyor Belt component on both versions for speed up/down

---
+++ Assets/Scripts/Stage/ConveyorBelt.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField, Tooltip("Extra speed provided by the belt when running on it. If negative, slows runner down.")]
+    private float extraSpeed = 5f;
+    public float ExtraSpeed => extraSpeed;
+}
