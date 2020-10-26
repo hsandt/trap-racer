62b126ae4c637f3aecb4d1c5e539a51d7c375ffd
huulong <n.huu.long@gmail.com>
2020-10-27 00:12:28 +0100

    [CAMERA] Force camera reset to target initial runner positions' center It fixes the bug of camera staying at the end and pushing all runners near the goal on restart, when countdown value was set to 0

--- Assets/Scripts/Race/RaceManager.cs
+++ Assets/Scripts/Race/RaceManager.cs
@@ -21,6 +21,9 @@ public class RaceManager : SingletonManager<RaceManager>
     [Tooltip("Spawn Point for character with flag (in front)")]
     public Transform spawnPointFlag;
     
+    /* Cached external references */
+    private InGameCamera m_InGameCamera;
+    
     /* State vars */
     
     /// Current state
@@ -43,6 +46,7 @@ public class RaceManager : SingletonManager<RaceManager>
     
     protected override void Init()
     {
+        m_InGameCamera = Camera.main.GetComponentOrFail<InGameCamera>();
         m_FlagTr = GameObject.FindWithTag(Tags.Flag).transform;
         RegisterRunners();
     }
@@ -53,7 +57,7 @@ public class RaceManager : SingletonManager<RaceManager>
     }
 
     private void SetupRace()
-    {
+    {        
         GateManager.Instance.SetupGates();
         DeviceManager.Instance.SetupDevices();
         
@@ -65,6 +69,9 @@ public class RaceManager : SingletonManager<RaceManager>
         
         int firstRunnerIndex = RandomlySortStartPositions();
         GiveFlagToRunner(firstRunnerIndex);
+        
+        // setup camera after spawning runners to target their initial position (including on Restart)
+        m_InGameCamera.Setup();
         
         StartUI.Instance.StartCountDown();
     }
