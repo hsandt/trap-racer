0bf4c348ee19437f5b7766314ddd7b73861af2ad
huulong <n.huu.long@gmail.com>
2020-11-14 20:00:11 +0100

    [LOBBY] Player can join with keyboard buttons

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using CommonsHelper;
 using UnityEngine;
 
 using CommonsPattern;
+using UnityEngine.InputSystem;
 
 public class LobbyUI : SingletonManager<LobbyUI>
 {
+    [SerializeField, Tooltip("Action needed to virtually join with keyboard for P1 " +
+                             "(no player is added, we count on PlayerInputKeyboard for keyboard input handling). " +
+                             "Keyboard equivalent of Join Action in PlayerManager, which is for Gamepad only.")]
+    private InputAction keyboardJoinActionP1 = new InputAction();
+    
+    [SerializeField, Tooltip("Same as above, but for P2 (array of InputActions has a bug that clones entries so we " +
+                             "don't use one).")]
+    private InputAction keyboardJoinActionP2 = new InputAction();
+    
     [Tooltip("Player join text game object array, by player index")]
     public GameObject[] playerJoinTextObjects;
     
@@ -17,14 +28,45 @@ public class LobbyUI : SingletonManager<LobbyUI>
 
     protected override void Init()
     {
+        keyboardJoinActionP1.performed += OnKeyboardJoinActionPerformedP1;
+        keyboardJoinActionP2.performed += OnKeyboardJoinActionPerformedP2;
+
         for (int playerIndex = 0; playerIndex < 2; playerIndex++)
         {
             playerJoinedWithKeyboardTextObjects[playerIndex].SetActive(false);
             playerJoinedWithGamepadTextObjects[playerIndex].SetActive(false);
         }
     }
+
+    private void OnEnable()
+    {
+        keyboardJoinActionP1.Enable();
+        keyboardJoinActionP2.Enable();
+    }
+
+    private void OnDisable()
+    {
+        keyboardJoinActionP1.Disable();
+        keyboardJoinActionP2.Disable();
+    }
+
+    private void OnKeyboardJoinActionPerformedP1(InputAction.CallbackContext ctx)
+    {
+        ConfirmPlayerJoinedWithKeyboard(0);
+    }
+
+    private void OnKeyboardJoinActionPerformedP2(InputAction.CallbackContext ctx)
+    {
+        ConfirmPlayerJoinedWithKeyboard(1);
+    }
+
+    private void ConfirmPlayerJoinedWithKeyboard(int playerIndex)
+    {
+        playerJoinTextObjects[playerIndex].SetActive(false);
+        playerJoinedWithKeyboardTextObjects[playerIndex].SetActive(true);
+    }
     
-    public void ConfirmPlayerJoined(int playerIndex)
+    public void ConfirmPlayerJoinedWithGamepad(int playerIndex)
     {
         playerJoinTextObjects[playerIndex].SetActive(false);
         playerJoinedWithGamepadTextObjects[playerIndex].SetActive(true);
