64d18607944af2c156e76f3e10a297d10185a4ca
komehara <komehara@gamedesignshort.com>
2020-12-13 15:50:23 +0100

    [UI] Select Play stage 1 button when showing StageSelectUI

--- Assets/Scripts/UI/StageSelectUI.cs
+++ Assets/Scripts/UI/StageSelectUI.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 using CommonsPattern;
+using UnityEngine.EventSystems;
 
 public class StageSelectUI : SingletonManager<StageSelectUI>
 {
-    // Load stage by number (1 or 2)
+    [Tooltip("First button to select")]
+    public GameObject firstSelected;
+    
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+    }
+    
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    /// Load stage by number (1 or 2)
+    /// Callback for Play stage X buttons
     public void LoadStage(int stageNumber)
     {
         SceneManager.LoadScene(stageNumber);
--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -6,16 +6,16 @@ public class TitleUI : SingletonManager<TitleUI>
 {
     /* Child references */
     
-    [Tooltip("StageSelectUI root")]
-    public GameObject stageSelectUI;
+    [Tooltip("StageSelectUI root script")]
+    public StageSelectUI stageSelectUI;
 
     private void Start()
     {
-        stageSelectUI.SetActive(false);
+        stageSelectUI.Hide();
     }
 
     public void ShowStageSelectUI()
     {
-        stageSelectUI.SetActive(true);
+        stageSelectUI.Show();
     }
 }
