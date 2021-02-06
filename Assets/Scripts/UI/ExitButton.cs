c7811b2e8604fac267a004e16bca60837f1a88c8
huulong <n.huu.long@gmail.com>
2021-02-06 16:00:49 +0100

    [UI] Added ExitButton and StandaloneOnly so we can exit game via button, except on WebGL

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    /// Callback for click action
+    public void OnClick()
+    {
+#if !UNITY_WEBGL
+        TitleUI.Instance.ExitGame();
+#endif
+    }
+}
